9f3c00bee697eb575bd819683b293163dd1db9b6
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-04-06 19:40:18 -0400

    Edit Activity done

--- src/forms/addActivity.js
+++ src/forms/addActivity.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { connect } from 'react-redux'
 
 
 class AddActivity extends React.Component {
@@ -42,43 +43,65 @@ class AddActivity extends React.Component {
             },
             body: JSON.stringify(data)
         }).then(resp => resp.json())
-        .then(response => { if (response.errors) { alert(response.errors) } })
+            .then(response => { if (response.errors) { alert(response.errors) } })
     }
 
+    // selectOptionsFunction = () => {
+    //     let newSelect = document.createElement('select')
+    //     newSelect.name="category_id" 
+    //     newSelect.value= this.state.value
+    //     newSelect.onChange= this.handleChange
+
+    //     for (let i = 0; i <= this.props.categories; i++) {
+    //         let category = this.props.categories[i]
+    //         let opt = document.createElement("option");
+    //         opt.value = i + 1;
+    //         opt.innerText = category.name.toUpperCase(); // whatever property it has
+
+    //         newSelect.appendChild(opt);
+    //         return opt
+    //     }
+
+    // }
 
 
-
-render() {
-    return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label >Name:</label>
-                <input onChange={this.handleChange} type="text" name="name" value={this.state.name}></input>
-                <label >ActivityType:</label>
-                <input onChange={this.handleChange} type="text" name="activityType" value={this.state.activityType}></input>
-                <label >Address:</label>
-                <input onChange={this.handleChange} type="text" name="address" value={this.state.address}></input>
-                <label >Image:</label>
-                <input onChange={this.handleChange} type="text" name="image" value={this.state.image}></input>
-                <label >Info:</label>
-                <input onChange={this.handleChange} type="text" name="about" value={this.state.about}></input>
-                <label >Choose a Category:</label>
-                <select name="category_id" value={this.state.value} onChange={this.handleChange}>
-                    <option value="1">Theaters</option>
-                    <option value="2">Sports</option>
-                    <option value="3">Nature</option>
-                    <option value="4">Art galleries and Museums</option>
-                </select>
-                <label >Date:</label>
-                <DatePicker selected={this.state.date} value={this.state.date} onChange={this.handleDateChange} />
-                {/* showTimeSelect dateFormat="Pp" */}
-                <button>AddActivity</button>
-            </form>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label >Name:</label>
+                    <input onChange={this.handleChange} type="text" name="name" value={this.state.name}></input>
+                    <label >ActivityType:</label>
+                    <input onChange={this.handleChange} type="text" name="activityType" value={this.state.activityType}></input>
+                    <label >Address:</label>
+                    <input onChange={this.handleChange} type="text" name="address" value={this.state.address}></input>
+                    <label >Image:</label>
+                    <input onChange={this.handleChange} type="text" name="image" value={this.state.image}></input>
+                    <label >Info:</label>
+                    <input onChange={this.handleChange} type="text" name="about" value={this.state.about}></input>
+                    <label >Choose a Category:</label>
+                    <select name="category_id" value={this.state.value} onChange={this.handleChange}>
+                        <option value="1">Theaters</option>
+                        <option value="2">Sports</option>
+                        <option value="3">Nature</option>
+                        <option value="4">Art galleries and Museums</option>
+                    </select>
+                    <label >Date:</label>
+                    <DatePicker selected={this.state.date} value={this.state.date} onChange={this.handleDateChange} />
+                    {/* showTimeSelect dateFormat="Pp" */}
+                    <button>AddActivity</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+const msp = (state) => {
+    return {
+       categories: state.categories 
+    }
 }
 
-export default AddActivity
+export default connect(msp)(AddActivity)
 
 
--- src/forms/editActivity.js
+++ src/forms/editActivity.js
@@ -4,14 +4,15 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 class EditActivity extends React.Component {
-    
-    
+
+
     state = {
         name: this.props.activity.name,
         activityType: this.props.activity.activityType,
         image: this.props.activity.image,
         about: this.props.activity.about,
         date: new Date(),
+        category_id: this.props.activity.category_id,
         address: this.props.activity.address
     }
 
@@ -28,53 +29,60 @@ class EditActivity extends React.Component {
     };
 
     handleSubmit = event => {
+        console.log('fetching 1');
+        
         event.preventDefault()
         let data = { ...this.state, category_id: parseInt(this.state.category_id) }
 
         console.log('fetching', data)
-        fetch('http://localhost:3000/activities', {
-            method: 'POST',
+        fetch(`http://localhost:3000/activities/${this.props.activity.id}`, {
+            method: 'PATCH',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                Accept: "application/json"
             },
             body: JSON.stringify(data)
         }).then(resp => resp.json())
-        .then(response => { if (response.errors) { alert(response.errors) } })
+            .then(response => {
+                if (response.errors) { alert(response.errors) }
+                else {
+                    console.log('fetching success');
+                }
+            })
     }
 
 
+    render() {
+        console.log(this.state);
+        return (
 
-render() {
-   console.log(this.state);
-    return (
-        
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label >Name:</label>
-                <input onChange={this.handleChange} type="text" name="name" value={this.state.name}></input>
-                <label >ActivityType:</label>
-                <input onChange={this.handleChange} type="text" name="activityType" value={this.state.activityType}></input>
-                <label >Address:</label>
-                <input onChange={this.handleChange} type="text" name="address" value={this.state.address}></input>
-                <label >Image:</label>
-                <input onChange={this.handleChange} type="text" name="image" value={this.state.image}></input>
-                <label >Info:</label>
-                <input onChange={this.handleChange} type="text" name="about" value={this.state.about}></input>
-                <label >Choose a Category:</label>
-                <select name="category_id" value={this.state.value} onChange={this.handleChange}>
-                    <option value="1">Theaters</option>
-                    <option value="2">Sports</option>
-                    <option value="3">Nature</option>
-                    <option value="4">Art galleries and Museums</option>
-                </select>
-                <label >Date:</label>
-                <DatePicker selected={this.state.date} value={this.state.date} onChange={this.handleDateChange} />
-                {/* showTimeSelect dateFormat="Pp" */}
-                <button>Edit Activity</button>
-            </form>
-        </div>
-    )
-}
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label >Name:</label>
+                    <input onChange={this.handleChange} type="text" name="name" value={this.state.name}></input>
+                    <label >ActivityType:</label>
+                    <input onChange={this.handleChange} type="text" name="activityType" value={this.state.activityType}></input>
+                    <label >Address:</label>
+                    <input onChange={this.handleChange} type="text" name="address" value={this.state.address}></input>
+                    <label >Image:</label>
+                    <input onChange={this.handleChange} type="text" name="image" value={this.state.image}></input>
+                    <label >Info:</label>
+                    <input onChange={this.handleChange} type="text" name="about" value={this.state.about}></input>
+                    <label >Choose a Category:</label>
+                    <select name="category_id" value={this.state.value} onChange={this.handleChange}>
+                        <option value="1">Theaters</option>
+                        <option value="2">Sports</option>
+                        <option value="3">Nature</option>
+                        <option value="4">Art galleries and Museums</option>
+                    </select>
+                    <label >Date:</label>
+                    <DatePicker selected={this.state.date} value={this.state.date} onChange={this.handleDateChange} />
+                    {/* showTimeSelect dateFormat="Pp" */}
+                    <button>Edit Activity</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EditActivity
