c4fa1987ffe25521cbb2dbfa5fb20fd7c5191dca
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-04-14 17:26:35 -0400

    initial styling commit

--- src/components/map.js
+++ src/components/map.js
@@ -11,7 +11,7 @@ class Map extends Component {
   }
 
   componentDidMount() {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address='${this.props.activity.address}'&key=`)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address='${this.props.activity.address}'&key=AIzaSyD4X3Xez83U_L3WZm6Fny8zsSxN_G4s1a4`)
       .then(resp => resp.json())
       .then(data => this.setState({
         lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng
@@ -25,7 +25,7 @@ class Map extends Component {
       <div style={{ height: '30vh', width: '50%' }}>
         {this.state.lat ?
           <GoogleMapReact
-            bootstrapURLKeys={{ key: '' }}
+            bootstrapURLKeys={{ key: 'AIzaSyD4X3Xez83U_L3WZm6Fny8zsSxN_G4s1a4' }}
             defaultCenter={{ lat: this.state.lat, lng: this.state.lng }}
             defaultZoom={this.state.zoom}
           >
