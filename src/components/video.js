25dbe4cb8d3e25c57836c54ec783dd119587337a
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-04-14 18:58:44 -0400

    welcome page stying w background video

---
+++ src/components/video.js
@@ -0,0 +1,44 @@
+import React from 'react'
+class Video extends React.Component {
+
+    state = {
+        loading: true
+    };
+    componentDidMount() {
+        if (this.video) {
+            this.video.addEventListener("loadeddata", () => {
+                this.setState({ loading: false });
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.video) {
+            this.video.removeEventListener("loadeddata", () => {
+            });
+        }
+    }
+
+
+    render() {
+        return (
+            <video
+                autoPlay
+                muted
+                loop
+                style={{
+                    position: "fixed",
+                    width: "100%",
+                    left: 0,
+                    top: 0,
+                    opacity: this.state.loading ? 0 : 1,
+                    transition: "opacity, 2s ease-in-out"
+                }}
+            >
+                <source src="public/Paris.mp4.mp4" type="video/mp4" />
+            </video>
+        );
+    }
+}
+
+export default Video 
--- src/containers/mainPage.js
+++ src/containers/mainPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../stylesheets/mainPage.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Signup from '../forms/signup';
-import Login from '../forms/login';
+// import Signup from '../forms/signup';
+// import Login from '../forms/login';
 
 
 class MainPage extends React.Component {
@@ -21,20 +21,20 @@ class MainPage extends React.Component {
     render() {
         return (
             <div className='main-page'>
-                {/* <p>MyActivities is here to help members all over the world to try new things with members who could be locals or tourists</p>
-                <p>The World is waiting...Let's Do Activities</p> */}
-                <video autoplay muted loop id="myVideo">
-                    <source src="public/Paris.mp4" type="video/mp4"/>
-                </video>
-                    {this.state.signup ? <div className='signup-combo'> <Signup history={this.props.history} /> <h6 className='h6-after-signup'>Member Already</h6> <button className='button-after-signup' onClick={this.changeSingupState}>Login</button></div> :
-                        <div> <Login /> <div className='login-combo' ><h6 className='h6-after-login'>New Member</h6> <button className='button-after-login' onClick={this.changeSingupState}>Sign Up</button> </div>} </div>}
                 
+                <video id='video1' autoPlay muted loop >
+                    Hello
+                    <source src='https://vod-progressive.akamaized.net/exp=1586918597~acl=%2A%2F1496953608.mp4%2A~hmac=f160bbf7e0c94ae3813ca89e3dd0c1e7a870e18db7e303dbd52f0efbdd4b5f73/vimeo-prod-skyfire-std-us/01/2747/14/363737105/1496953608.mp4' type='video/mp4' />
+                </video>
+
             </div>
-                )
-            }
-        }
-        
-        
-        export default MainPage
-        
-        
+        )
+    }
+}
+
+
+export default MainPage
+
+
+// {this.state.signup ? <div className='signup-combo'> <Signup history={this.props.history} /> <h6 className='h6-after-signup'>Member Already</h6> <button className='button-after-signup' onClick={this.changeSingupState}>Login</button></div> :
+//                     <div> <Login /> <div className='login-combo' ><h6 className='h6-after-login'>New Member</h6> <button className='button-after-login' onClick={this.changeSingupState}>Sign Up</button> </div>} </div>}
